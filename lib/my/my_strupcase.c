bc3558850c4edae3872b3fbbda337229b52daa4d
SobshDev <s.sobsh@proton.me>
2025-04-08 13:30:00 +0200

    Implement my_strupcase

---
+++ lib/my/my_strupcase.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** Project Name
+** File description:
+** Project Description
+*/
+
+char *my_strupcase(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+    }
+    return str;
+}
