603104f19c7346ee507cc5ade864677237b22893
SobshDev <s.sobsh@proton.me>
2025-05-19 14:30:00 +0200

    Implement create_if_node

---
+++ src/parser/create_if_node.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** 42sh
+** File description:
+** create_if_node
+*/
+
+#include "parser.h"
+
+node_t *create_if_node(node_t *condition, node_t *then_branch)
+{
+    node_t *node = create_empty_node();
+
+    if (!node)
+        return NULL;
+    node->type = NODE_IF;
+    node->left = condition;
+    node->right = then_branch;
+    return node;
+}
