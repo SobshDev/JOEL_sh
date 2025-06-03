9a42b4894a5fa42f9978c053eabf98271e2baef5
SobshDev <s.sobsh@proton.me>
2025-06-03 08:30:00 +0200

    Add execute_and

---
+++ src/core/execution/execute_and.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** minishell2
+** File description:
+** execute_and
+*/
+
+#include "minishell2.h"
+
+/*
+* Executes the logical AND operation between two commands
+* If the first command succeeds, the second command is executed.
+* If the first command fails, the second command is not executed.
+*/
+void execute_and(shell_t *shell, node_t *node)
+{
+    bool prev_logic_context = shell->in_logic_context;
+
+    shell->in_logic_context = true;
+    execute_ast(shell, node->left);
+    shell->in_logic_context = prev_logic_context;
+    if (shell->exit_value == 0) {
+        shell->in_logic_context = true;
+        execute_ast(shell, node->right);
+        shell->in_logic_context = prev_logic_context;
+    }
+}
