6880842362f6567b08d30d77037259458437c872
SobshDev <s.sobsh@proton.me>
2025-06-10 14:00:00 +0200

    Implement ask_chatgpt

---
+++ src/core/key/ask_chatgpt.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** B-PSU-200-MPL-2-1-42sh-louis.bagnol
+** File description:
+** ask_chatgpt
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "minishell2.h"
+#include "chatgpt.h"
+
+char *ask_chatgpt(char *message, shell_t *shell)
+{
+    (void)message;
+    (void)shell;
+    return NULL;
+}
